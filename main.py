297230c90f4589f4a11b77969204c006927424b3
mathiasmunene <mathiasmunene@gmail.com>
2025-07-02 21:05:48 +0300

    main. py 3rd

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ from lib.tasks import load_taks, save_tasks, add_task, view_tasks,
 def main():
     tasks = load_taks()
 
-    
+    while True:
+        print("\nTo-do List Application")
+        print("1. View Task")
+        print("2. Add Task")
+        print("3. Complete Task")
+        print("4. Delete Task")
+        print("5. Exit")
